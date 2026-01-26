3207ea6b770feaa4464baa74207ced649a4514af
scrapychain <mindquaker20@gmail.com>
2026-01-25 19:16:29 -0500

    updated package.json and enxt.config.js files

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
+  basePath: process.env.NODE_ENV === 'production' ? '/ashutosh-rana-dev' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/ashutosh-rana-dev/' : '',
 }
 
 module.exports = nextConfig
