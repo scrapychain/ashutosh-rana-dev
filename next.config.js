38c4697928a00f217038f833d534d3b0d6e058c2
scrapychain <mindquaker20@gmail.com>
2026-01-25 19:34:58 -0500

    upadeted next.config.js

--- next.config.js
+++ next.config.js
@@ -5,8 +5,7 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: "/ashutosh-rana-dev",
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/ashutosh-rana-dev/' : '',
+  
 }
 
 module.exports = nextConfig
